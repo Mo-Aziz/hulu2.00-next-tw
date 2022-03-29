08c6e5eaf3a0020602d3cd3bf75cd976c3d6e0f5
MJ <mohamed.aref32@gmail.com>
2022-03-29 17:44:04 -0500

    finished styling the nav bar with mmovie section, intiated the result page for all movies

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,27 @@
-import Head from 'next/head'
- 
-import Header from '../components/Header'
- 
+import Head from "next/head";
+
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <Head>
         <title>Hulu 2.00</title>
         <meta name="description" content="clone of hulu 2.00" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
- 
-       {/* Header */}
-       <Header/>
-       {/* Nav */}
-       {/* result */}
+
+      {/* Header */}
+      <Header />
+      {/* Nav */}
+      <Nav />
+      {/* result */}
+      <Results/>
     </div>
-  )
+  );
+
+  // server side rendering for mdb data
 }
+
