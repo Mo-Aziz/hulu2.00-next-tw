c29413584ce37aa6e7d68647c1e309ec6cfcd252
MJ <mohamed.aref32@gmail.com>
2022-04-18 19:02:09 -0500

    added the result component and final thumbnails

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,12 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({results}) {
+
+  console.log("The movies====>",results);
+  
   return (
     <div>
       <Head>
@@ -18,10 +22,40 @@ export default function Home() {
       {/* Nav */}
       <Nav />
       {/* result */}
-      <Results/>
+      <Results results={results}/>
     </div>
   );
-
-  // server side rendering for mdb data
 }
 
+  // server side rendering for tmdb data
+
+export async function getServerSideProps(context){
+const genre = context.query.genre;
+
+const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`).then((res) => res.json());
+
+return{
+  props:{
+    results: request.results
+  }
+}
+}
+
+
+// export async function getServerSideProps(context) {
+//   const genre = context.query.genre;
+// results: [];
+  
+//   const request = await fetch(`
+//   https://api.themoviedb.org/3${
+//     requests[genre]?.url || requests.fetchTrending.url
+//   }`)
+
+//   const results = await request.json();
+
+//   return {
+//     props: {
+//       results: results
+//     },
+//   };
+// }
