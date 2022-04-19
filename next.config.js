d17337b365f5a3da7981ec0f142e5ee03c785291
MJ <mohamed.aref32@gmail.com>
2022-04-18 19:40:06 -0500

    disabling eslint

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images:{
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
+   
     domains:[  "links.papareact.com",
     "www.hulu.com" ,
     "encrypted-tbn0.gstatic.com",
